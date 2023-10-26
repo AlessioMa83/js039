aa35a6b9d417f612d490f12a5846f0ae003ce069
AlessioMa83 <63257149+AlessioMa83@users.noreply.github.com>
2023-10-26 19:51:07 +0200

    js

---
+++ 26-10/app.js
@@ -0,0 +1,98 @@
+/* 
+le variabili 
+
+scatotalaNome = Alessio
+scatolaAnni = 40
+scatolaCitta = Lecce
+
+i tipi da dato
+
+Stringhe = ' utf-8 😁 ☻ ○ Ì á q {} '
+Numeri = 1 2 3 4 5 6 7 8 9 0  integer numeri interi
+nuemro = 0.56 flat
+numeri negativi = -1 -2 -3 -4 -5 -6 -7 -8 -9 -0
+
+Booleani = true false 1 0 
+ posso prendere la patente
+
+vedoBene = true
+ho 18Anni = true
+vedoBne  = false
+darkMode = true
+
+Array = [1,2,3,4,5,6,7,8,9,0]
+
+ogetto = {
+    nome: 'Alessio',
+    anni: 40,
+    citta: 'Lecce',
+    array: [1,2,3,4,5,6,7,8,9,0],
+    indirizzo: {via:roma,
+                numero: 5,
+                citta: 'Lecce'
+                }
+
+}
+
+bigInt = 9007199254740991n
+bigInt = 10n
+
+undefined = undefined
+null = null
+NaN = NaN /* not a number typeof Number  */
+
+
+
+
+let scatolaNome = 'Alessio'
+let arr = [2, 5, 5, 'stringhe', true.false, [3, 3, 4, 5, 7], scatolaNome]
+
+
+
+let num = Number.MAX_SAFE_INTEGER
+let numNegativo = Number.MIN_SAFE_INTEGER
+console.log(num, numNegativo);
+console.log(typeof num);
+console.log(typeof 10n);
+console.log(2998e8);
+console.log(299800000000);
+
+ let scatolaVuota2;/* memorizzo */
+ console.log(typeof scatolaVuota2);
+scatolaVuota2 = 'ciao'/* assegnando */
+console.log(typeof scatolaVuota2);
+console.log(typeof NaN);
+console.log(15 * 'ciao');
+
+/* var scatolaVuota;
+var scatolaVuota ='ciao' */
+
+/* dichiarazione delle variabili */
+
+/* let / cosnt / var */
+
+/* for contatto 
+nome
+cognome 
+indirizzo
+*/
+
+
+let nome = 'Alessio'/* definito da chi compila il form */
+/* let 3nome */
+let _nome = 'Alessio'
+let $nome = 'Alessio'
+let nome3 = 'Alessio'
+let no5me = 'Alessio'
+/* let nome-cognome = 'Alessio'
+let var = 'Alessio' */
+let qualSiasiCosa; /* undefine */
+/* const Maggio; *//* constanti */
+const cognome = 'maggio'/* stringa */
+
+
+const indirizzo = 'via roma';/* non psoso modificare un acostante */
+/* indirizzo = 'via lecce'; */
+
+console.log( (10 + (+'10')));/* php 20 */
+
